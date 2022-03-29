289c32c6758f19f04a62bc45c81dc25769e367f3
Alex <older.miller.ua@gmail.com>
2022-03-29 15:27:25 +0200

    Employees add form input functional added

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,29 +1,53 @@
+import { Component } from 'react';
+
 import './employees-add-form.css';
 
-const EmployeesAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex"
-            >
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?"
-                />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" 
-                />
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        };
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light"
+    onValueChange = event => {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    render() {
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex"
                 >
-                    Добавить
-                </button>
-            </form>
-        </div>
-    );
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?"
+                        name="name"
+                        value={name}            
+                        onChange={this.onValueChange}
+                    />
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $?"
+                        name="salary"
+                        value={salary}        
+                        onChange={this.onValueChange}
+                    />
+    
+                    <button type="submit"
+                            className="btn btn-outline-light"
+                    >
+                        Добавить
+                    </button>
+                </form>
+            </div>
+        );
+    }
 };
 
 export default EmployeesAddForm;
